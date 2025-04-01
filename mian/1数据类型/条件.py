a729b68c1a37ce96a146f73c3cea84cb6be462ec
gollum <1105026730@qq.com>
2025-04-01 23:55:58 +0800

    版本修改

--- mian/1数据类型/条件.py
+++ mian/1数据类型/条件.py
@@ -8,10 +8,10 @@ else:
     print("3")
 
 # py3.10版本之后才会有, case _表示匹配所有
-match num :
-    case 1:
-        print("1")
-    case 2:
-        print("2")
-    case _:
-        print("3")
+# match num :
+#     case 1:
+#         print("1")
+#     case 2:
+#         print("2")
+#     case _:
+#         print("3")
