f9f055f488e49687673ca6a4bd6cca1700d1de0b
sherlock <1105026730@qq.com>
2025-04-02 15:04:30 +0800

    random

--- mian/4模块/random_test.py
+++ mian/4模块/random_test.py
@@ -11,3 +11,10 @@ print(r)
 # 获取列表中的随机元素
 list1 = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 print(list1[random.randint(0, len(list1) - 1)])
+
+# 随机获取元素
+print(random.choice(list1))
+
+# 打乱元素
+random.shuffle(list1)
+print(list1)
